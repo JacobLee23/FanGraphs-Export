ffc05c5c2c88800cccd66c06d099d6b9f8716a03
Jacob Lee <JLpython@outlook.com>
2021-03-01 18:27:31 -0800

    Initialize classes

---
+++ FanGraphs/leaders.py
@@ -0,0 +1,42 @@
+#! python3
+# FanGraphs/leaders.py
+
+"""
+
+"""
+
+
+class MajorLeagueLeaderboards:
+
+    def __init__(self):
+        pass
+
+
+class SplitsLeaderboards:
+
+    def __init__(self):
+        pass
+
+
+class SeasonStatGrid:
+
+    def __init__(self):
+        pass
+
+
+class GameSpanLeaderboards:
+
+    def __init__(self):
+        pass
+
+
+class KBOLeaderboards:
+
+    def __init__(self):
+        pass
+
+
+class WARLeaderboards:
+
+    def __init__(self):
+        pass
