f323011f6e8b6fe2d76aeb8d1dbd290cf663a9f4
Jacob Lee <JLpython@outlook.com>
2021-03-01 18:39:53 -0800

    Add __init__ to MajorLeagueLeaderboards

--- FanGraphs/leaders.py
+++ FanGraphs/leaders.py
@@ -5,11 +5,22 @@
 
 """
 
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
 
 class MajorLeagueLeaderboards:
-
+    """ Parse FanGraphs >> Leaders >> Major League Leaderboards
+"""
     def __init__(self):
-        pass
+        self.__options = Options()
+        self.__options.headless = True
+        self.browser = webdriver.Firefox(
+            options=self.__options
+        )
+
+    def quit(self):
+        self.browser.quit()
 
 
 class SplitsLeaderboards:
