9b75e9f5a22c04bdf772ef130845a331ad3cde89
Jacob Lee <JLpython@outlook.com>
2021-03-25 08:11:51 -0700

    Add method docstrings

--- FanGraphs/leaders.py
+++ FanGraphs/leaders.py
@@ -1025,11 +1025,25 @@ class WARLeaderboards(ScrapingUtilities):
 
     @classmethod
     def list_queries(cls):
+        """
+        Lists the possible filter queries which can be used to modify search results.
+
+        :return: Filter queries which can be used to modify search results
+        :rtype: list
+        """
         queries = []
         queries.extend(list(cls.__dropdowns))
         return queries
 
     def list_options(self, query: str):
+        """
+        Lists the possible options which a filter query can be configured to.
+
+        :param query: The filter query
+        :return: Options which the filter query can be configured to
+        :rtype: list
+        :raises FanGraphs.exceptions.InvalidFilterQuery: Invalid argument ``query``
+        """
         query = query.lower()
         if query in self.__dropdowns:
             elems = self.soup.select(
@@ -1041,6 +1055,14 @@ class WARLeaderboards(ScrapingUtilities):
         return options
 
     def current_option(self, query: str):
+        """
+        Retrieves the option which a filter query is currently set to.
+
+        :param query: The filter query being retrieved of its current option
+        :return: The option which the filter query is currently set to
+        :rtype: str
+        :raises FanGraphs.exceptions.InvalidFilterQuery: Invalid argument ``query``
+        """
         query = query.lower()
         if query in self.__dropdowns:
             elem = self.soup.select(self.__dropdowns[query])[0]
@@ -1050,6 +1072,13 @@ class WARLeaderboards(ScrapingUtilities):
         return option
 
     def configure(self, query: str, option: str):
+        """
+        Configures a filter query to a specified option.
+
+        :param query: The filter query to be configured
+        :param option: The option to set the filter query to
+        :raises FanGraphs.exceptions.InvalidFilterQuery: Invalid argument ``query``
+        """
         query = query.lower()
         self._close_ad()
         if query in self.__dropdowns:
@@ -1059,6 +1088,13 @@ class WARLeaderboards(ScrapingUtilities):
         self._refresh_parser(waitfor=self.__waitfor)
 
     def __configure_dropdown(self, query: str, option: str):
+        """
+        Configures a dropdown-class filter query to an option.
+
+        :param query: The dropdown-class filter query to be configured
+        :param option: The option to set the filter query to
+        :raises FanGraphs.exceptions.InvalidFilterOption: Invalid argument ``option``
+        """
         options = self.list_options(query)
         try:
             index = options.index(option)
@@ -1071,6 +1107,14 @@ class WARLeaderboards(ScrapingUtilities):
         elem.click()
 
     def export(self, path=""):
+        """
+        Uses the **Export Data** button on the webpage to export the current leaderboard.
+        The data will be exported as a CSV file and the file will be saved to *out/*.
+        The file will be saved to the filepath ``path``, if specified.
+        Otherwise, the file will be saved to the filepath *./out/%d.%m.%y %H.%M.%S.csv*
+
+        :param path: The path to save the exported data to
+        """
         self._close_ad()
         if not path or os.path.splitext(path)[1] != ".csv":
             path = "out/{}.csv".format(
