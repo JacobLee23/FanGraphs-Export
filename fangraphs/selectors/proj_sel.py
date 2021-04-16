60ace89bae14f30e162fb69abd0e1d52ce0b769a
Jacob Lee <JLpython@outlook.com>
2021-04-16 11:24:35 -0700

    Rework classes in fangraphs.selectors.proj_sel

--- fangraphs/selectors/proj_sel.py
+++ fangraphs/selectors/proj_sel.py
@@ -1,6 +1,8 @@
 #! python3
 # FanGraphs/selectors/proj_sel.py
 
+from fangraphs import selectors
+
 """
 CSS selectors for the classes in :py:mod:`fangraphs.projections`.
 """
@@ -10,19 +12,21 @@ class Projections:
     """
     CSS selectors for :py:class:`fangraphs.projections.Projections`.
     """
-    selections = {
+    __selections_type_1 = {
         "stat": "#ProjectionBoard1_tsStats",
         "position": "#ProjectionBoard1_tsPosition",
         "projection": "#ProjectionBoard1_tsProj",
         "update": "#ProjectionBoard1_tsUpdate"
     }
-    dropdowns = {
-        "team": "#ProjectionBoard1_rcbTeam_Input",
-        "league": "#ProjectionBoard1_rcbLeague_Input",
-    }
-    dropdown_options = {
-        "team": "#ProjectionBoard1_rcbTeam_DropDown",
-        "league": "#ProjectionBoard1_rcbLeague_DropDown"
+    __dropdowns_type_1 = {
+        "team": ("#ProjectionBoard1_rcbTeam_Input", "#ProjectionBoard1_rcbTeam_DropDown"),
+        "league": ("#ProjectionBoard1_rcbLeague_Input", "#ProjectionBoard1_rcbLeague_DropDown")
     }
     waitfor = ""
     export_data = "#ProjectionBoard1_cmdCSV"
+
+    def __init__(self, page):
+        for key, val in self.__selections_type_1.items():
+            setattr(self, key, selectors.SelectionsType1(page, val))
+        for key, val in self.__dropdowns_type_1.items():
+            setattr(self, key, selectors.DropdownsType1(page, *val))
