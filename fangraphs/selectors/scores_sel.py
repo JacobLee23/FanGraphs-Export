0519d9d73a84e37303859875ddff757bf5fec925
Jacob Lee <JLpython@outlook.com>
2021-05-27 14:21:28 -0700

    Delete CSS selectors for fangraphs.tests.Live (no configurations)

--- fangraphs/selectors/scores_sel.py
+++ fangraphs/selectors/scores_sel.py
@@ -12,19 +12,5 @@ class Live:
     """
     CSS selectors for :py:class:`fangraphs.leaders.Live`.
     """
-    __dropdowns_type_1 = {
-        "season": ("#LiveBoard1_rcbSeason_Input", "#LiveBoard1_rcbSeason_DropDown")
-    }
-    __calendars = {
-        "date": (
-            "#LiveBoard1_rdpDate_popupButton",
-            "#LiveBoard1_rdpDate_calendar",
-            "#LiveBoard1_rdpDate_dateInput_wrapper"
-        )
-    }
-
-    def __init__(self, page):
-        for key, val in self.__dropdowns_type_1.items():
-            self.__setattr__(key, selectors.DropdownsType1(page, *val))
-        for key, val in self.__calendars.items():
-            self.__setattr__(key, selectors.Calendars(page, *val))
+    def __init__(self):
+        pass
