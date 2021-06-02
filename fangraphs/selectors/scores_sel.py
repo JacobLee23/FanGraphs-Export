72aadc1fb7e585929d85b633adbb18e9b1554cc0
Jacob Lee <JLpython@outlook.com>
2021-06-02 15:24:09 -0700

    Write unit tests; Compile CSS selectors

--- fangraphs/selectors/scores_sel.py
+++ fangraphs/selectors/scores_sel.py
@@ -10,7 +10,7 @@ from fangraphs import selectors
 
 class Live:
     """
-    CSS selectors for :py:class:`fangraphs.leaders.Live`.
+    CSS selectors for :py:class:`fangraphs.scores.Live`
     """
     waitfor = ""
     export_data = ""
@@ -21,7 +21,7 @@ class Live:
 
 class LiveLeaderboards:
     """
-    CSS selectors for :py:class:`fangraphs.leaders.LiveLeaderboards`.
+    CSS selectors for :py:class:`fangraphs.scores.LiveLeaderboards`
     """
     __selections_type_4 = {
         "player_type": ".playertypes",
@@ -35,3 +35,29 @@ class LiveLeaderboards:
     def __init__(self, page):
         for key, val in self.__selections_type_4.items():
             self.__setattr__(key, selectors.SelectionsType4(page, val))
+
+
+class Scoreboard:
+    """
+    CSS selectors for :py:class:`fangraphs.scores.Scoreboard`
+    """
+    __calendars = {
+        "date": (
+            "#LiveBoard1_rdpDate_popupButton",
+            "#LiveBoard1_rdpDate_calendar",
+            "#LiveBoard1_rdpDate_dateInput_wrapper"
+        )
+    }
+    __dropdowns_type_1 = {
+        "season": (
+            "#LiveBoard1_rcbSeason",
+            "#LiveBoard1_rcbSeason_DropDown"
+        )
+    }
+
+    waitfor = ""
+    export_data = ""
+
+    def __init__(self, page):
+        for key, val in self.__calendars.items():
+            self.__setattr__(key, selectors.Calendars(page, *val))
